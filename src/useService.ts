02a115098dcf0d3366e65922e687400cd9f1b46b
“chrisshank” <chris.shank.23@gmail.com>
2021-07-19 22:46:27 -0700

    refactor(getSnapshot): update useActor and useService for getSnapshot

--- src/useService.ts
+++ src/useService.ts
@@ -4,37 +4,34 @@ import { Ref, isRef } from '@vue/composition-api';
 
 import { useActor } from './useActor';
 
-export function getServiceSnapshot<TService extends Interpreter<any, any, any, any>>(
-	service: TService
-): TService['state'] {
-	// TODO: remove compat lines in a new major, replace literal number with InterpreterStatus then as well
-	return ('status' in service ? service.status : (service as any)._status) !== 0
-		? service.state
-		: service.machine.initialState;
-}
-
+/**
+ * @deprecated Use `useActor` instead.
+ *
+ * @param service The interpreted machine
+ * @returns A tuple of the current `state` of the service and the service's `send(event)` method
+ */
 export function useService<
-	TContext,
-	TEvent extends EventObject,
-	TTypestate extends Typestate<TContext> = { value: any; context: TContext }
+  TContext,
+  TEvent extends EventObject,
+  TTypestate extends Typestate<TContext> = { value: any; context: TContext }
 >(
-	service:
-		| Interpreter<TContext, any, TEvent, TTypestate>
-		| Ref<Interpreter<TContext, any, TEvent, TTypestate>>
+  service:
+    | Interpreter<TContext, any, TEvent, TTypestate>
+    | Ref<Interpreter<TContext, any, TEvent, TTypestate>>
 ): {
-	state: Ref<State<TContext, TEvent, any, TTypestate>>;
-	send: Sender<TEvent>;
+  state: Ref<State<TContext, TEvent, any, TTypestate>>;
+  send: Sender<TEvent>;
 } {
-	if (
-		process.env.NODE_ENV !== 'production' &&
-		!('machine' in (isRef(service) ? service.value : service))
-	) {
-		throw new Error(
-			`Attempted to use an actor-like object instead of a service in the useService() hook. Please use the useActor() hook instead.`
-		);
-	}
+  if (
+    process.env.NODE_ENV !== 'production' &&
+    !('machine' in (isRef(service) ? service.value : service))
+  ) {
+    throw new Error(
+      `Attempted to use an actor-like object instead of a service in the useService() hook. Please use the useActor() hook instead.`
+    );
+  }
 
-	const { state, send } = useActor(service, getServiceSnapshot);
+  const { state, send } = useActor(service);
 
-	return { state, send };
+  return { state, send };
 }
