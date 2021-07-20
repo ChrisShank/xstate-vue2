84ec9fd17328629fbe34c9b4a829ad551d24e133
“chrisshank” <chris.shank.23@gmail.com>
2021-07-19 23:17:13 -0700

    feat(useSpawn): add useSpawn composable to spaen behaviors

---
+++ test/UseSpawn.vue
@@ -0,0 +1,29 @@
+<template>
+  <button data-testid="count" @click="send({ type: 'INC' })">
+    {{ count }}
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import { useActor, useSpawn } from '../src';
+import { fromReducer } from 'xstate/lib/behaviors';
+
+const reducer = (state: number, event: { type: 'INC' }): number => {
+  console.log(state, event);
+  if (event.type === 'INC') {
+    return state + 1;
+  }
+  return state;
+};
+
+const behavior = fromReducer(reducer, 0);
+
+export default defineComponent({
+  setup() {
+    const actorRef = useSpawn(behavior);
+    const { state: count, send } = useActor(actorRef);
+    return { count, send };
+  }
+});
+</script>
